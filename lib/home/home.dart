75870b47660c79b584de9b4d299a1565bfdcc428
unknown <brettzky007@gmail.com>
2023-10-04 19:48:14 -0500

    Added Firebase Auth

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fluttercourse/login/login.dart';
+// import 'package:fluttercourse/shared/shared.dart';
+import 'package:fluttercourse/topics/topics.dart';
+import 'package:fluttercourse/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: ElevatedButton(
-        child: Text("topics"),
-        onPressed: () => Navigator.pushNamed(context, '/topics'),
-      ),
-    ));
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("loading");
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('error'),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
   }
 }
